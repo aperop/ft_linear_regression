59d515cdb541bef0fdb06cc710b1ffeb49ce2191
Ildar <dhawkgir@student.21-school.ru>
2022-10-19 19:34:14 +0300

    req

--- venv.sh
+++ venv.sh
@@ -1,4 +1,3 @@
-python3 -m venv venv
-source venv/bin/activate
-pip install -U pip
-pip install -r requirements.txt
+python3.10 -m venv venv & source venv/bin/activate;
+pip install -U pip;
+pip install -r requirements.txt;
