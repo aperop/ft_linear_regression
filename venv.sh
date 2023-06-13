3ed86125f1ab697ed7296b623c12646777f70460
Python <ildarnyrt@gmail.com>
2023-06-13 19:48:33 +0300

    refactoring

--- venv.sh
+++ venv.sh
@@ -1,1 +1,1 @@
-python3.10 -m venv venv && source venv/bin/activate && pip install -U pip && pip install -r requirements.txt
+python3 -m venv venv && source venv/bin/activate && pip install -U pip && pip install -r requirements.txt
